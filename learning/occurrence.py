e77db7e5bebea8d0e5319f947092b1ae7b6c8e96
Henrique Cavalcante <henrique.pcavalcante@gmail.com>
2018-05-27 22:16:59 -0400

    Add occurrence.py
    Signed-off-by: Henrique Cavalcante <henrique.pcavalcante@gmail.com>

---
+++ learning/occurrence.py
@@ -0,0 +1,12 @@
+class Occurrence:
+    '''
+    Represent the terms that occur in the Knowledge Base
+
+    Occurrences have the following properties:
+        term: A string representing the occurrence name.
+        frequency: A counter representing the number of occurrences of a term.
+    '''
+
+    def __init__(self, term):
+        self.term = term
+        self.frequency = 1
